cbcb409997befc01825794fabdc370a526688ec6
abc7468 <abc7468@naver.com>
2021-12-02 02:00:31 +0900

    tucker curri

---
+++ myapp/app.go
@@ -0,0 +1,69 @@
+package myapp
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+type User struct {
+	ID        int       `json:"id"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+var userMap map[int]*User
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello world")
+}
+
+func usersHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Get UserInfo by /users/{id}")
+}
+
+func getUserInfoHandler(w http.ResponseWriter, r *http.Request) {
+	user := new(User)
+	user.ID = 2
+	user.FirstName = "jang"
+	user.LastName = "seonghyun"
+	user.Email = "abc@naver.com"
+
+	w.Header().Add("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	data, _ := json.Marshal(user)
+	fmt.Fprint(w, string(data))
+}
+
+func createUserHandler(w http.ResponseWriter, r *http.Request) {
+	user := new(User)
+	err := json.NewDecoder(r.Body).Decode(user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, err)
+		return
+	}
+
+	user.ID = 2
+	user.CreatedAt = time.Now()
+	w.Header().Add("content-type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	data, _ := json.Marshal(user)
+	fmt.Fprint(w, string(data))
+}
+
+func NewHandler() http.Handler {
+	userMap = make(map[int]*User)
+
+	mux := mux.NewRouter()
+	mux.HandleFunc("/", indexHandler)
+	mux.HandleFunc("/users", usersHandler).Methods("GET")
+	mux.HandleFunc("/users", createUserHandler).Methods("POST")
+	mux.HandleFunc("/users/{id:[0-9]+}", getUserInfoHandler)
+	return mux
+}
---
+++ myapp/app_test.go
@@ -0,0 +1,90 @@
+package myapp
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"strconv"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIndex(t *testing.T) {
+	assert := assert.New(t)
+	ts := httptest.NewServer(NewHandler())
+	defer ts.Close()
+
+	resp, err := http.Get(ts.URL)
+	assert.NoError(err)
+	assert.Equal(http.StatusOK, resp.StatusCode)
+	data, _ := ioutil.ReadAll(resp.Body)
+	assert.Equal("Hello world", string(data))
+}
+
+func TestUsers(t *testing.T) {
+	assert := assert.New(t)
+	ts := httptest.NewServer(NewHandler())
+	defer ts.Close()
+
+	resp, err := http.Get(ts.URL + "/users")
+	assert.NoError(err)
+	assert.Equal(http.StatusOK, resp.StatusCode)
+	data, _ := ioutil.ReadAll(resp.Body)
+	assert.Contains(string(data), "Get UserInfo")
+}
+
+func TestGetUserInfo(t *testing.T) {
+	assert := assert.New(t)
+	ts := httptest.NewServer(NewHandler())
+	defer ts.Close()
+
+	resp, err := http.Get(ts.URL + "/users/89")
+	assert.NoError(err)
+	assert.Equal(http.StatusOK, resp.StatusCode)
+	data, _ := ioutil.ReadAll(resp.Body)
+	assert.Equal(string(data), "No User Id:89")
+
+	resp, err = http.Get(ts.URL + "/users/56")
+	assert.NoError(err)
+	assert.Equal(http.StatusOK, resp.StatusCode)
+	data, _ = ioutil.ReadAll(resp.Body)
+	assert.Equal(string(data), "User Id:56")
+}
+
+func TestCreateUser(t *testing.T) {
+	assert := assert.New(t)
+	ts := httptest.NewServer(NewHandler())
+	defer ts.Close()
+	res, err := http.Post(ts.URL+"/users", "application/json",
+		strings.NewReader(`{"first_name":"jang", "last_name":"seonghyun","email":"abc@naver.com"}`))
+	assert.NoError(err)
+	assert.Equal(http.StatusCreated, res.StatusCode)
+
+	user := new(User)
+	err = json.NewDecoder(res.Body).Decode(user)
+	assert.NoError(err)
+	assert.NotEqual(0, user.ID)
+
+	id := user.ID
+	res, err = http.Get(ts.URL + "/users/" + strconv.Itoa(id))
+	assert.NoError(err)
+	assert.Equal(http.StatusOK, res.StatusCode)
+
+	user2 := new(User)
+	err = json.NewDecoder(res.Body).Decode(user2)
+	assert.NoError(err)
+	assert.Equal(user.ID, user2.ID)
+	assert.Equal(user.FirstName, user2.FirstName)
+}
+
+func TestCreateUserWithNoData(t *testing.T) {
+	assert := assert.New(t)
+	ts := httptest.NewServer(NewHandler())
+	defer ts.Close()
+	res, err := http.Post(ts.URL+"/users", "application/json", nil)
+	assert.NoError(err)
+	assert.Equal(http.StatusBadRequest, res.StatusCode)
+}
